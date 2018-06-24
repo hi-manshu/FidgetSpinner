99beeccef031be6d80b4272ab79b3e99e009c9f1
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-06-24 20:22:05 +0530

    Added: Language Preference

--- app/src/main/java/fidgetspinner/himanshusingh/xyz/go/activities/LanguagePreference.kt
+++ app/src/main/java/fidgetspinner/himanshusingh/xyz/go/activities/LanguagePreference.kt
@@ -4,17 +4,31 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import fidgetspinner.himanshusingh.xyz.go.utility.PreferenceHelper
+import kotlinx.android.synthetic.main.activity_language_preference.*
 import xyz.himanshusingh.fidgetspinner.R
+import xyz.himanshusingh.fidgetspinner.activities.onboarding.LoginActivity
 
 
 class LanguagePreference : AppCompatActivity() {
+    
     companion object {
         fun sendToLanguagePreferenceActivity(context: Context): Intent {
             return Intent(context, LanguagePreference::class.java)
         }
     }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_preference)
+        setupUI()
+        
+    }
+    
+    private fun setupUI() {
+        english_radio.setOnClickListener {}
+        hindi_radio.setOnClickListener {}
+        lets_go_button.setOnClickListener {startActivity(LoginActivity.sendToLoginActivity(this@LanguagePreference))}
+    
     }
 }
---
+++ app/src/main/java/fidgetspinner/himanshusingh/xyz/go/utility/ToastUtils.kt
@@ -0,0 +1,2 @@
+package fidgetspinner.himanshusingh.xyz.go.utility
+
